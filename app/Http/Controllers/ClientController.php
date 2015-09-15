23a1621b6d4d5ba94021bdaf72d170067dc1444e
Luiz Carlos <luizgadao@gmail.com>
2015-09-15 11:09:10 -0300

    first commit

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Client;
+use Illuminate\Http\Request;
+
+use CodeProject\Http\Requests;
+use CodeProject\Http\Controllers\Controller;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return Client::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        return Client::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $client = Client::find($id);
+
+        return $client;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        $client = Client::find($id);
+        if (isset($client)){
+            $client->update($request->all());
+            return $client;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        return Client::destroy($id);
+    }
+}
